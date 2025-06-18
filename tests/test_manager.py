21118014a00e7bb9d40534d3408de358a2087ce8
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2025-06-18 16:30:14 -0700

    wrote tests for contact model

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -3,12 +3,28 @@ from contacts.models import Contact
 
 class TestContact(unittest.TestCase):
     def test_contact_initialization(self):
-        os.chdir(BASE_PATH + "/")
         contact = Contact("Alice", "Smith", "4642336443", "alice@website.com")
         self.assertEqual(contact.first_name, "Alice")
-        self.assertEquals(contact.last_name, "Smith")
-        self.assertEquals(contact.number, "4642336443")
-        self.assertEquals(contact.email, "alice@website.com")
+        self.assertEqual(contact.last_name, "Smith")
+        self.assertEqual(contact.number, "4642336443")
+        self.assertEqual(contact.email, "alice@website.com")
+        
+    def test_contact_equality(self):
+        c1 = Contact("Bob", "Smith")
+        c2 = Contact("Bob", "Smith")
+        c1.id = c2.id # only for testing
+        self.assertEqual(c1, c2)
+        
+    def test_contact_less_than(self):
+        c1 = Contact("Bob", "Smith", 4633464644, "bob@bob.com")
+        c2 = Contact("Alice", "Tufts", 3532335443, "yuh@alice.com")
+        c3 = Contact("Alice", "Tufts", 3452335323, "other_alice@tufts.com")
+        c2.id = 0 # only for testing
+        c3.id = 1 # only for testing
+        
+        self.assertEqual(c1 > c2, True)
+        self.assertEqual(c1 < c2, False)
+        self.assertEqual(c3 > c2, True)
         
 if __name__ == "__main__":
     unittest.main()
